0253aed19631a77b38be4bbda1ba9997ef6cc89b
gnori-zon <konasenkos@gmail.com>
2023-05-17 22:35:39 +0300

    create project

---
+++ src/main/java/org/gnori/votingsystemrest/VotingSystemRestApplication.java
@@ -0,0 +1,13 @@
+package org.gnori.votingsystemrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSystemRestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VotingSystemRestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/gnori/votingsystemrest/VotingSystemRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gnori.votingsystemrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSystemRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
