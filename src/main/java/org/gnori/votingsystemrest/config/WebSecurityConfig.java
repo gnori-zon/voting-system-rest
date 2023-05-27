8bb615b2391433c408bab7cfae2dd6bdb216f5de
gnori-zon <konasenkos@gmail.com>
2023-05-27 20:05:11 +0300

    - added two dependencies (...starter-security.../...security-test...) - added WebSecurityConfig - added method in UserDao - impl UserDetails in UserEntity

--- src/main/java/org/gnori/votingsystemrest/config/WebSecurityConfig.java
+++ src/main/java/org/gnori/votingsystemrest/config/WebSecurityConfig.java
@@ -4,10 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.gnori.votingsystemrest.model.entity.enums.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,14 +14,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-  private final AuthenticationProvider authProvider;
-
-  private final AuthenticationEntryPoint authEntryPoint;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
+        .csrf().disable()
         .authorizeHttpRequests(
             requests -> requests
                 .requestMatchers("/api/v1/admin/**").hasRole(Role.ADMIN.name())
@@ -30,9 +27,7 @@ public class WebSecurityConfig {
                 .requestMatchers("/api/v1/auth/**").permitAll()
                 .anyRequest().authenticated()
     )
-        .authenticationProvider(authProvider)
-        .exceptionHandling()
-        .authenticationEntryPoint(authEntryPoint);
+        .httpBasic(Customizer.withDefaults());
 
     return http.build();
 
