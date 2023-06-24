81aea4b5945d8d0749d62b508a96beb43234203d
gnori-zon <konasenkos@gmail.com>
2023-06-24 18:11:40 +0300

    - fixed profiling controllers - added conditional for profiling

--- src/main/java/org/gnori/votingsystemrest/log/MethodExecutionTimeLogAspect.java
+++ src/main/java/org/gnori/votingsystemrest/log/MethodExecutionTimeLogAspect.java
@@ -4,28 +4,30 @@ import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Log4j2
 @Component
+@ConditionalOnProperty("profiling.all.controllers")
 public class MethodExecutionTimeLogAspect {
 
-  private static final String DEBUG_TEXT_EXECUTION_TIME = "Method: |{}| executed in {} ms";
+  private static final String INFO_TEXT_EXECUTION_TIME = "Method: |{}| executed in {} ms";
 
   @Around("@annotation(org.gnori.votingsystemrest.log.annotation.LogMethodExecutionTime)")
   public Object logMethodExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable{
 
     var startTime = System.currentTimeMillis();
 
-    var executionResult = joinPoint.proceed();
+    try {
+      return joinPoint.proceed();
 
-    var executionTime = System.currentTimeMillis() - startTime;
+    }  finally {
+      var executionTime = System.currentTimeMillis() - startTime;
 
-    log.debug(DEBUG_TEXT_EXECUTION_TIME, joinPoint.getSignature(),  executionTime);
-
-    return executionResult;
-
+      log.info(INFO_TEXT_EXECUTION_TIME, joinPoint.getSignature(),  executionTime);
+    }
   }
 
 }
